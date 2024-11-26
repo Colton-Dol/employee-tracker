4271daa6aba179dd2b60791e8142775513359b83
Colton Doling <cmdoling@gmail.com>
2024-11-26 14:29:23 -0500

    fixed several syntax issues in schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE role (
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id),
+    FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
 
@@ -40,8 +40,8 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    FOREIGN KEY (role_id),
+    FOREIGN KEY (role_id)
     REFERENCES role(id),
-    manager_id INT,
+    manager_id INT
     REFERENCES employee(id)
 );
