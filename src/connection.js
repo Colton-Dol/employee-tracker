f64c5d39172957f14b1a10e745650945ba9b3dd1
Colton Doling <cmdoling@gmail.com>
2024-11-25 21:19:02 -0500

    added connectToDb function to connection.js and exported it alongside the pool variable

--- src/connection.js
+++ src/connection.js
@@ -6,3 +6,14 @@ const { Pool } = pg;
 const pool = new Pool ({
     uri: process.env.POSTGRES_URI
 });
+
+const connectToDb = async () => {
+    try {
+        await pool.connect();
+    } catch (error) {
+        console.error('Error connecting to database:', error);
+        process.exit(1);
+    }
+};
+
+export { pool, connectToDb };
