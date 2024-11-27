f06097bc1045eae74edd1ff45822f1c87b13d0c9
Colton Doling <cmdoling@gmail.com>
2024-11-26 21:18:53 -0500

    added department_id to role insertion

--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -5,13 +5,13 @@ VALUES
     ('Finance'),
     ('Legal');
 
-INSERT INTO role (title, salary)
+INSERT INTO role (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000),
-    ('Salesperson', 80000),
-    ('Lead Engineer', 150000),
-    ('Software Engineer', 120000),
-    ('Account Manager', 160000),
-    ('Accountant', 125000),
-    ('Legal Team Lead', 250000),
-    ('Lawyer', 190000);
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
