d608b993faee2eb6964ea935ff7550a05321c2e8
Colton Doling <cmdoling@gmail.com>
2024-12-01 14:33:29 -0500

    added choices to the inquirer prompt

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ inquirer
             name: 'action',
             message: 'What would you like to do?',
             choices: [
-                
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add Department',
+                'Add Role',
+                'Add Employee',
+                'Update Employee Role'
             ]
         }
     ])
