f7a13e37ddf2c8d7d1ecd4b681adb4730a55ab62
Colton Doling <cmdoling@gmail.com>
2024-12-01 15:27:39 -0500

    filled in the viewDepartments function and exported the Queries class

--- src/queries.js
+++ src/queries.js
@@ -3,7 +3,13 @@ import { pool } from "./connection.js";
 class Queries {
     
     viewDepartments() {
-
+        pool.query('SELECT * FROM department', (err, res) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(res.rows)
+            }
+        })
     }
 
     viewRoles() {
@@ -30,3 +36,5 @@ class Queries {
         
     }
 }
+
+export default new Queries();
