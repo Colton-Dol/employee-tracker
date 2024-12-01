ca3ec1a174fb698a20cd96556f5e4c2143880c01
Colton Doling <cmdoling@gmail.com>
2024-12-01 15:12:34 -0500

    added a queries class to queries.js and added some empty functions for each choice in the index prompt

--- src/queries.js
+++ src/queries.js
@@ -1,1 +1,32 @@
 import { pool } from "./connection.js";
+
+class Queries {
+    
+    viewDepartments() {
+
+    }
+
+    viewRoles() {
+
+    }
+
+    viewEmployees() {
+
+    }
+
+    addDepartment() {
+
+    }
+
+    addRole() {
+
+    }
+
+    addEmployee() {
+
+    }
+
+    updateEmployeeRole() {
+        
+    }
+}
