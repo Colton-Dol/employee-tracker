74875e6c6c2062c6f3ad610991b89d272f220c97
Colton Doling <cmdoling@gmail.com>
2024-12-02 15:01:14 -0500

    filled in the addEmployee method, choices in the addEmployee prompt are blank while figuring out how to dynamically present choices

--- src/queries.js
+++ src/queries.js
@@ -92,7 +92,46 @@ class Queries {
     }
 
     addEmployee() {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'firstName',
+                    message: `What is the employee's first name?`
+                },
+                {
+                    type: 'input',
+                    name: 'lastName',
+                    message: `What is the employee's last name?`
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: `What is the employee's role?`,
+                    choices: [
+                        
+                    ]
+                },
+                {
+                    type: 'list',
+                    name: 'manager',
+                    message: `Who is the employee's manager?`,
+                    choices: [
+                        'None',
 
+                    ]
+                }
+            ])
+            .then((answers) => {
+                pool.query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES
+                    ('${answers.firstName}', '${answers.lastName}', '${answers.role}', '${answers.manager}')`, (err, res) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log(`Added ${answers.firstName} ${answers.lastName} to the database`);
+                        }
+                    })
+            })
     }
 
     updateEmployeeRole() {
